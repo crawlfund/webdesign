407f48c466c095507e73ce597611cde358dd4ae3
luyang00 <393780933@qq.com>
2016-05-12 00:58:26 +0800

    add login process not complet process

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 DATABASE = 'database/data.db'
 app = Flask(__name__)
@@ -10,15 +10,26 @@ def get_db():
     return db
 
 
-@app.route('/')
+@app.route('/index')
 def show_entries():
     cur = get_db().cursor()
-    cur.execute('SELECT * FROM login')
+    cur.execute('SELECT * FROM tbl')
     print cur.fetchall()
     return render_template('index.html')
+def valid_login(username,password):
+    print 'username:'+username,'password:'+password
+    return 1
 
-
-@app.route('/login')
+@app.route('/login_process', methods=['GET', 'POST'])
+def login_process():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],request.form['password']):
+            return render_template('index.html')
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
+@app.route('/')
 def login():
     return render_template('login.html')
 
