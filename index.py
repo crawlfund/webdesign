a827dd7957c54c8b4604e6cf038e917ca7b6706b
luyang00 <393780933@qq.com>
2016-05-12 01:44:18 +0800

    add a user login mechanism

--- index.py
+++ index.py
@@ -10,23 +10,25 @@ def get_db():
     return db
 
 
-@app.route('/index')
+@app.route('/va')
 def show_entries():
     cur = get_db().cursor()
-    cur.execute('SELECT * FROM tbl')
-    print cur.fetchall()
-    return render_template('index.html')
+    #cur.execute('SELECT * FROM tbl')
+    #print cur.fetchall()
+    return render_template('index.html',valid=False)
 
 def valid_login(username,password):
-    print 'username:'+username,'password:'+password
-    return 1
+    if username==u'root' and password ==u'root':
+        return True
+    else:
+        return False
 
-@app.route('/login_process', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login_process():
     error = None
     if request.method == 'POST':
         if valid_login(request.form['username'],request.form['password']):
-            return render_template('index.html')
+            return render_template('index.html',valid=True)
         else:
             error = 'Invalid username/password'
     return render_template('login.html', error=error)
