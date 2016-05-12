c66601e7f5a9a0df15ed7229b2793fa414d54211
luyang00 <393780933@qq.com>
2016-05-12 12:33:43 +0800

    add function qiuzhu&qichuang&weisheng&etc

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import sqlite3
 from flask import Flask,render_template,request
 
@@ -36,6 +38,35 @@ def login_process():
 def login():
     return render_template('login.html')
 
+@app.route('/chiyao')
+def chiyao():
+    cur=get_db().cursor()
+    cur.execute('SELECT id,name,chiyao FROM information')
+    title=u'服药情况'
+    table_head=[u'编号',u'姓名',u'是否已服药']
+    return render_template('index.html',valid=True,title=title,table_head=table_head,content=cur.fetchall())
 
+@app.route('/weisheng')
+def weisheng():
+    cur=get_db().cursor()
+    cur.execute('SELECT id,name,weisheng FROM information')
+    title=u'卫生情况'
+    table_head=[u'编号',u'姓名',u'是否已打扫']
+    return render_template('index.html',valid=True,title=title,table_head=table_head,content=cur.fetchall())
+
+@app.route('/qiuzhu')
+def qiuzhu():
+    cur=get_db().cursor()
+    cur.execute('SELECT id,name,qiuzhu FROM information')
+    title=u'求助情况'
+    table_head=[u'编号',u'姓名',u'是否需要帮助']
+    return render_template('index.html',valid=True,title=title,table_head=table_head,content=cur.fetchall())
+@app.route('/qichuang')
+def qichuang():
+    cur=get_db().cursor()
+    cur.execute('SELECT id,name,qichuang FROM information')
+    title=u'起床情况'
+    table_head=[u'编号',u'姓名',u'是否已起床']
+    return render_template('index.html',valid=True,title=title,table_head=table_head,content=cur.fetchall())
 if __name__ == '__main__':
     app.run(debug=True)
